3621f524be1e7f890767f638bae2f62364713e67
Sherry1481 <shareyscorpi1@gmail.com>
2021-06-01 03:58:35 +0500

    5 Collection View cell

--- Fashion App/ViewController.swift
+++ Fashion App/ViewController.swift
@@ -16,11 +16,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView.collectionViewLayout = layout
+        collectionView.isPagingEnabled = true
+        
         
     }
 
     @IBAction func nextButtonTapped(_ sender: Any) {
     }
+    
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        return cell
+    }
+    
+    
+    
     
 }
 
